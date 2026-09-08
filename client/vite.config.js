aff2b37af69d241a0c29045a50265bd7de21f3f8
Nida Fatima <nidaftma688@gmail.com>
2026-09-08 11:21:14 +0500

    initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5052',
+        target: 'http://localhost:5053',
         changeOrigin: true,
       },
     },
